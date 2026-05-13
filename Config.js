dbd8dad4d5684662045709ec7e03d8fae4b63a5d
Lᴜᴄɪғᴇʀ Dᴇᴍᴏɴ <191839344+scamer0018@users.noreply.github.com>
2026-05-14 01:33:12 +0530

    config.json

---
+++ Config.js
@@ -0,0 +1,25 @@
+{
+  "bot_token": "8963304419:AAGsCETZeFv6rLBxeeg-7zt0ONwF4ld18gQ",
+  "bot_name": "PREMIUM FORCE JOIN BOT",
+  "owner_username": "@Lucifer_Co_Online",
+  "admins": [8533520806],
+  "force_channels": [
+    "@Next_DYS"
+  ],
+  "buttons": [
+    {
+      "name": "Updates",
+      "url": "https://t.me/YOUR_CHANNEL"
+    },
+    {
+      "name": "Support",
+      "url": "https://t.me/YOUR_GROUP"
+    },
+    {
+      "name": "Website",
+      "url": "https://google.com"
+    }
+  ],
+  "start_image": "https://i.imgur.com/8fK4h6F.jpeg",
+  "force_image": "https://i.imgur.com/0Z8FQWf.jpeg"
+}
